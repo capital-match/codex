11a205ce0240d24d403cf2675902c6ae13e08890
cydparser <cydparser@gmail.com>
2018-10-28 02:14:40 -0700

    Only use Stack when .stack-work is present
    Alleviates #86

--- codex/Main.hs
+++ codex/Main.hs
@@ -159,7 +159,8 @@ main = withSocketsDo $ do
       TaggerNotFound  -> fail' $ "codex: tagger not found."
       Ready           -> do
         stackFileExists <- doesFileExist $ "." </> "stack.yaml"
-        if stackFileExists then do
+        stackWorkExists <- doesFileExist $ "." </> ".stack-work"
+        if stackFileExists && stackWorkExists then do
             (ec, _, _) <- readCreateProcessWithExitCode (shell "which stack") ""
             case ec of
                 ExitSuccess -> do
